56085cbad473c0c0c5bbb5ccca6f1ba84b7a9fd9
ltk2801 <luutuankhanha3dt@gmail.com>
2022-04-21 12:19:26 +0700

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require ('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware')
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port =process.env.PORT || 5000 
+
+connectDB()
 
 const app = express()
 
@@ -11,4 +15,5 @@ app.use(express.urlencoded( { extended:false } ))
 app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.use(errorHandler)
+
 app.listen(port, () => console.log(`Server started on port ${port} `))
