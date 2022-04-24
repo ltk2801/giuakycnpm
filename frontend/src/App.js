402158ec1bdccfdddb089f9ef52bd168fd6b4d4d
ltk2801 <luutuankhanha3dt@gmail.com>
2022-04-24 13:57:59 +0700

    Frontend Authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
@@ -21,13 +23,15 @@ function App() {
         /> <
         Route path = '/login'
         element = { < Login / > }
-        /> <
+        />  <
         Route path = '/register'
         element = { < Register / > }
-        /> <
-        /Routes> <
-        /div> <
+        />  <
+        /Routes>  <
+        /div>  <
         /Router> <
+        ToastContainer / >
+        <
         />
     )
 }
