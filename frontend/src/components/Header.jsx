728749551e6ee6ce32ab752ebbb4e4387a42faa7
ltk2801 <luutuankhanha3dt@gmail.com>
2022-04-24 22:01:00 +0700

    Prepare deploy

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -15,30 +15,35 @@ function Header() {
         navigate('/')
     }
 
-  return (
-    <header className='header'>
-        <div className="logo">
-            <Link to ='/'>GoalSetter</Link>
-        </div>
-        <ul>
+    return (
+        <header className='header'>
+          <div className='logo'>
+            <Link to='/'>GoalSetter</Link>
+          </div>
+          <ul>
             {user ? (
-        <li>
-            <button className='btn' onClick={onLogout}>
-                <FaSignOutAlt /> Logout
-            </button>
-        </li>
-        ) : (
-        <>
-        <li>
-            <Link to ='/Register'>
-                <FaUser /> Register
-            </Link>
-        </li>
-            </>)}
-            
-        </ul>
-    </header>
-  )
-}
+              <li>
+                <button className='btn' onClick={onLogout}>
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            ) : (
+              <>
+                <li>
+                  <Link to='/login'>
+                    <FaSignInAlt /> Login
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/register'>
+                    <FaUser /> Register
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </header>
+      )
+    }
 
 export default Header
